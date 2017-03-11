5f0931ba0ffbcb5b61a018c925ae85195905ae11
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-03-11 13:12:38 -0500

    added state

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -1,15 +1,15 @@
-let renderSelector = (id, data)=> {
-  $(id).empty();
-  var dropdown = data.map(function(dataObject){
-    return "<option>"+dataObject.name+"</option>";
-  });
-  $(id).append(dropdown.join(''));
+let renderSelector = (id, data) => {
+    $(id).empty();
+    var dropdown = data.map(function(dataObject) {
+        return "<option>" + dataObject.name + "</option>";
+    });
+    $(id).append(dropdown.join(''));
 }
 
-function renderAll(){
-  renderSelector('#hotel-choices', hotels);
-  renderSelector('#restaurant-choices', restaurants);
-  renderSelector('#activity-choices', activities);
-};
+function renderAll() {
+    renderSelector('#hotel-choices', hotels);
+    renderSelector('#restaurant-choices', restaurants);
+    renderSelector('#activity-choices', activities);
+}
 
 renderAll();
