c4a058983f356cdcb18b99721ac5ca83744e69f6
jordancclive <jordancclive@gmail.com>
2017-03-11 12:58:21 -0500

    completed renderSelector

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -1,1 +1,9 @@
-console.log("abc123string");
+let renderSelector = (id, data)=> {
+  $(id).empty();
+  var dropdown = data.map(function(dataObject){
+    return "<option>"+dataObject.name+"</option>";
+  });
+  $(id).append(dropdown.join(''));
+}
+
+renderSelector('#hotel-choices', hotels);
