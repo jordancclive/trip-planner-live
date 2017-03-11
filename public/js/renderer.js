219b0a714bb39dbdd9ee07949164b1a92ade504a
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-03-11 15:19:30 -0500

    made add day function

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -6,15 +6,14 @@ let renderSelector = (id, data, mode, updateDayFnc) => {
 
     var button = $(id).siblings('button');
     button.on('click', function(){
-        updateDayFnc(id, data, mode);
+        updateDayFnc(id, mode);
     });
     $(id).append(dropdown.join(''));
 };
 
-const updateDay = function(id, data, mode){
+const updateDay = function(id, mode){
     var selectedIndex = $(id + ' option:selected').index();
-    var selectedData = data[selectedIndex];
-    state.days[state.selectedDayIdx][mode] = selectedData;
+    state.days[state.selectedDayIdx][mode] = selectedIndex;
 }
 
 const renderDaySelector = (className, idx) => {
@@ -22,17 +21,32 @@ const renderDaySelector = (className, idx) => {
         if (index === idx){
             return `<button class="btn btn-circle day-btn current-day">${index + 1}</button>`;
         }
-        return `<button class="btn btn-circle day-btn">${index + 1}</button>`;
+        return `<button class="btn btn-circle day-btn btn-func">${index + 1}</button>`;
     });
+
+    selectors.push('<button class="btn btn-circle day-btn" id="day-add">+</button>');
+
     $(className).empty();
     $(className).append(selectors.join(''));
+};
+
+const addDay = function(){
+    state.days.push({});
+    renderDaySelector('.day-buttons', state.selectedDayIdx);
+};
+
+const wireUpDaySelector = function(className){
+     $(className).on('click', '#day-add', function(){
+         addDay();
+     });
 };
 
 function renderAll() {
     renderSelector('#hotel-choices', hotels, 'hotel', updateDay);
     renderSelector('#restaurant-choices', restaurants, 'resturant', updateDay);
     renderSelector('#activity-choices', activities, 'activity', updateDay);
-    renderDaySelector('.day-buttons', state.selectedDayIdx);
+    renderDaySelector('.day-buttons', state.selectedDayIdx, addDay);
+    wireUpDaySelector('.day-buttons');
 }
 
 renderAll();
