c6e0892cfb4b6a973eebbdc9b6ba61a4c330ecab
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-03-11 13:21:27 -0500

    made day selector

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -6,10 +6,22 @@ let renderSelector = (id, data) => {
     $(id).append(dropdown.join(''));
 }
 
+const renderDaySelector = (className, idx) => {
+    const selectors = state.days.map((day, index) => {
+        if (index === idx){
+            return `<button class="btn btn-circle day-btn current-day">${index + 1}</button>`;
+        }
+        return `<button class="btn btn-circle day-btn">${index + 1}</button>`;
+    });
+    $(className).empty();
+    $(className).append(selectors.join(''));
+};
+
 function renderAll() {
     renderSelector('#hotel-choices', hotels);
     renderSelector('#restaurant-choices', restaurants);
     renderSelector('#activity-choices', activities);
+    renderDaySelector('.day-buttons', state.selectedDayIdx);
 }
 
 renderAll();
